fe761e3e962641234ccfc3a2f56a08847d7ce326
Egos808 <134227361+Egos808@users.noreply.github.com>
2024-10-09 00:10:10 -0600

    Add files via upload

---
+++ estudiantes.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM estudiantes
+LIMIT 0, 1000
+
+-- Date: 2024-10-09 00:09
+*/
+INSERT INTO `` (`id`,`nombre`,`actividades`,`primer_parcial`,`segundo_parcial`,`examen_final`,`puntaje_total`) VALUES (4,'Mario Salazar',26,13,5,25,69.00);
+INSERT INTO `` (`id`,`nombre`,`actividades`,`primer_parcial`,`segundo_parcial`,`examen_final`,`puntaje_total`) VALUES (5,'Lalo Salamanca',15,15,15,15,60.00);
+INSERT INTO `` (`id`,`nombre`,`actividades`,`primer_parcial`,`segundo_parcial`,`examen_final`,`puntaje_total`) VALUES (16,'Walter White',35,15,15,35,100.00);
